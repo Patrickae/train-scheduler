5b33dc68294deffbb8047042ac9f75cf05541970
Andy <Andy@Admins-MacBook-Air.local>
2017-04-02 15:49:16 -0400

    scheduler working

---
+++ assets/javascript/script.js
@@ -0,0 +1,103 @@
+  
+
+
+var config = {
+	apiKey: "AIzaSyAdEHOdishKsrxIfSTh1t6e9CWUaQP4-0U",
+	authDomain: "fir-800c4.firebaseapp.com",
+	databaseURL: "https://fir-800c4.firebaseio.com",
+	projectId: "fir-800c4",
+	storageBucket: "fir-800c4.appspot.com",
+	messagingSenderId: "508246552950"
+};
+
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+$("#submit").on("click", function(){
+
+	event.preventDefault();
+
+	var impTrain = $("#train-name").val().trim();
+	var impDest = $("#destination").val().trim();
+	var impArrTime = moment($("#first-train-time").val().trim(), "HH:mm").format("X");
+	var impFrequency = $("#frequency").val().trim();
+
+	console.log(impTrain);
+	console.log(impFrequency);
+	console.log(impDest);
+	console.log(impArrTime);
+
+
+	var newImp = {
+		train: impTrain,
+		dest: impDest,
+		firstArrival: impArrTime,
+		frequency : impFrequency
+	}
+
+	database.ref().push(newImp);
+
+	$("input").val("");
+
+});
+
+
+
+database.ref().on("child_added", function(snapshot){
+
+	console.log(snapshot.val());
+
+	var train = snapshot.val().train;
+	var dest = snapshot.val().dest;
+	var firstArrival = snapshot.val().firstArrival;
+	var frequency = snapshot.val().frequency;
+
+	console.log(train);
+	console.log(dest);
+	console.log(firstArrival);
+	console.log(frequency);
+
+	var timeDiff = moment().diff(moment.unix(firstArrival, "X"), "minutes")
+
+	console.log(timeDiff);
+
+	var nextArr = moment.unix(firstArrival).format("h:mm a");
+
+	
+
+	 var nextArr;
+
+	if(timeDiff > 0){
+
+		var remainder = timeDiff % frequency;
+
+		console.log(remainder);
+
+		nextArr = moment().add(remainder, "minutes").format("X");
+
+		console.log(nextArr);
+
+	}
+	else{
+		 nextArr = firstArrival;
+
+		 console.log(nextArr);
+	};
+
+	var minsAway = 0 - moment().diff(moment.unix(nextArr, "X"), "minutes");
+	console.log(nextArr);
+	console.log(minsAway);
+
+	nextArrPretty = moment.unix(nextArr).format("h:mm a");
+	console.log(nextArrPretty);
+
+$("#table > tbody").append("<tr><td>" + train + "</td><td>" + dest + "</td><td>" + frequency + "</td><td>" + nextArrPretty + "</td><td>" + minsAway + "</td><td><button class='btn btn-default'>X</button></td></tr>");
+
+
+});
+
+
+
